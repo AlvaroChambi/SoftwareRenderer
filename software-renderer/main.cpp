9327851675d5c0ecb7c3734a6ba5bc098fad0410
achambi <alvaro.chambi.campos@gmail.com>
2015-12-05 11:04:12 +0100

    Initial Commit

---
+++ software-renderer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  software-renderer
+//
+//  Created by Alvaro Chambi Campos on 5/12/15.
+//  Copyright © 2015 Alvaro Chambi Campos. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
